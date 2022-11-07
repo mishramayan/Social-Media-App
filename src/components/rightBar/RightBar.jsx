47540e5225034c151f00cfc112d9103dc589e43f
Mayan Mishra <mishra.ma@northeastern.edu>
2022-11-06 21:07:21 -0500

    Working on right bar

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -3,8 +3,124 @@ import './rightBar.scss'
 
 const RightBar = () => {
   return (
-    <div>
-      Rightbar
+    <div className='rightBar'>
+      <div className="container">
+        <div className="item">
+          <span>Suggestions</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <span>Mayan Mishra</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <span>Mayan Mishra</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <p>
+                <span>Mayan Mishra</span> changed their profile picture.
+              </p>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <p>
+                <span>Mayan Mishra</span> changed their cover photo.
+              </p>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <p>
+                <span>Mayan Mishra</span> commented on your profile picture.
+              </p>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <p>
+                <span>Mayan Mishra</span> liked your cover photo.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <div className='online'/>
+              <span>Mayan Mishra</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <div className='online'/>
+              <span>Mayan Mishra</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <div className='online'/>
+              <span>Mayan Mishra</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <div className='online'/>
+              <span>Mayan Mishra</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <div className='online'/>
+              <span>Mayan Mishra</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="Profile" /> 
+              <div className='online'/>
+              <span>Mayan Mishra</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
