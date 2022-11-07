0b46301c0ac7ef59930a150bbaf3bb68c6e4cf58
Mayan Mishra <mishra.ma@northeastern.edu>
2022-11-06 19:37:08 -0500

    Working on left bar

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,10 +1,51 @@
 import './leftBar.scss'
 import React from 'react'
+import Friends from "../../assets/1.png";
+import Groups from "../../assets/2.png";
+import Market from "../../assets/3.png";
+import Watch from "../../assets/4.png";
+import Memories from "../../assets/5.png";
+import Events from "../../assets/6.png";
+import Gaming from "../../assets/7.png";
+import Gallery from "../../assets/8.png";
+import Videos from "../../assets/9.png";
+import Messages from "../../assets/10.png";
+import Tutorials from "../../assets/11.png";
+import Courses from "../../assets/12.png";
+import Fund from "../../assets/13.png";
 
 const LeftBar = () => {
   return (
     <div className='leftBar'>
-      LeftBar
+      <div className='container'>
+        <div className="menu">
+          <div className="user">
+            <img src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+            alt="Profile" /> 
+            <span>Mayan Mishra</span>
+          </div>
+          <div className="item">
+            <img src={Friends} alt="" />
+            <span>Friends</span>
+          </div>
+          <div className="item">
+            <img src={Groups} alt="" />
+            <span>Groups</span>
+          </div>
+          <div className="item">
+            <img src={Market} alt="" />
+            <span>Marketplace</span>
+          </div>
+          <div className="item">
+            <img src={Watch} alt="" />
+            <span>Watch</span>
+          </div>
+          <div className="item">
+            <img src={Memories} alt="" />
+            <span>Memories</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
